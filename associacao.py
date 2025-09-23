0cfb54cca0362aa720d0b6870a1902ce83c0bc6d
jhonbslv <jhonbslv@gmail.com>
2025-09-23 10:17:56 -0300

    carroseassociaçõe

--- associacao.py
+++ associacao.py
@@ -43,3 +43,28 @@ motor3 = Motor(3.0, "BMW")
 
 roda1 = Roda(17, "Pirelli")
 roda2 = Roda(18, "Michelin")
+
+carro1 = Carro()
+carro2 = Carro()
+carro3 = Carro()
+carro4 = Carro()
+carro5 = Carro()
+carro6 = Carro()
+
+carro1.set_motor(motor1)
+carro1.set_rodas([roda1, roda1, roda1, roda1])
+
+carro2.set_motor(motor1)
+carro2.set_rodas([roda2, roda2, roda2, roda2])
+
+carro3.set_motor(motor2)
+carro3.set_rodas([roda1, roda1, roda1, roda1])
+
+carro4.set_motor(motor2)
+carro4.set_rodas([roda2, roda2, roda2, roda2])
+
+carro5.set_motor(motor3)
+carro5.set_rodas([roda1, roda1, roda1, roda1])
+
+carro6.set_motor(motor3)
+carro6.set_rodas([roda2, roda2, roda2, roda2])
